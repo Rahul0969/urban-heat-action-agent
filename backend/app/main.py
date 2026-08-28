68d0413862ee45430c1596d97e132567814f4790
Rahul9690 <rahuldubey0969@gmail.com>
2026-08-28 10:37:07 +0530

    Ready to use

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+
+        "https://urban-heat-action-agent.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
